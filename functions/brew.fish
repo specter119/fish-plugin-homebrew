49ff52b5f52afc9a08f02344e5f118e958bea248
specter119 <specter119@gmail.com>
2019-02-25 15:41:20 +0800

    try to fix brew

--- functions/brew.fish
+++ functions/brew.fish
@@ -1,5 +1,5 @@
 # Defined in - @ line 1
 function brew -d "Modify Path of brew [remove pyenv shims]"
 	type -q pyenv && set -lx PATH (string replace (pyenv root)/shims '' $PATH);
-	brew doctor;
+	brew $argv;
 end
