506ae567dda9820bccff9b5eaa8563525fbc91d8
specter119 <specter119@gmail.com>
2019-03-06 19:03:11 +0800

    fix bub{c,o} functions

--- functions/bubc.fish
+++ functions/bubc.fish
@@ -1,7 +1,8 @@
 function bubc --description 'Upgrade the outdated formula and cask then cleanup'
   switch (uname)
-  case Darwin
-    brew upgrade && brew cask upgrade && brew cleanup
-  case Linux
-    brew upgrade && brew cleanup
+    case Darwin
+      brew upgrade && brew cask upgrade && brew cleanup
+    case Linux
+      brew upgrade && brew cleanup
+  end
 end
--- functions/bubo.fish
+++ functions/bubo.fish
@@ -1,7 +1,8 @@
 function bubo --description 'Update homebrew and show outdated formula and cask'
   switch (uname)
-  case Darwin
-    brew update && brew outdated && brew cask outdated
-  case Linux
-    brew update && brew outdated
+    case Darwin
+      brew update && brew outdated && brew cask outdated
+    case Linux
+      brew update && brew outdated
+  end
 end
