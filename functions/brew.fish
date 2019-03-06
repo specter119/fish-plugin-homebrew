a3aed711b103d03ef2d4d28a23f01e186ea36b39
specter119 <specter119@gmail.com>
2019-03-06 15:10:31 +0800

    update brew for doctor work with conda and pbs

--- functions/brew.fish
+++ functions/brew.fish
@@ -1,4 +1,6 @@
-function brew --description 'A missing package manager for macOS'
-  type -q pyenv && set -lx PATH (string replace (pyenv root)/shims '' $PATH);
+function brew --description 'Package manager for macOS or Linux'
+  set -q CONDA_PREFIX; set -l PATH (string replace $CONDA_PREFIX/bin '' $PATH)
+  type -q pbs-config; set -l PATH (string replace (pbs-config --prefix)/bin '' $PATH)
+  type -q pyenv;set -l PATH (string replace (pyenv root)/shims '' $PATH)
   command brew $argv;
 end
