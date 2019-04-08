29ef486c7ab514bd32a2fb62a2a0269110359003
specter119 <specter119@gmail.com>
2019-04-08 11:13:20 +0800

    make brew.fish more fish like

--- conf.d/brew.fish
+++ conf.d/brew.fish
@@ -1,6 +1,6 @@
 function brew --description 'Package manager for macOS or Linux'
-  set -q CONDA_PREFIX && set -lx PATH (string match -v $CONDA_PREFIX/bin $PATH)
-  type -q pbs-config && set -lx PATH (string match -v (pbs-config --prefix)/bin $PATH)
-  type -q pyenv && set -lx PATH (string match -v (pyenv root)/shims $PATH)
+  set -q CONDA_PREFIX; and set -lx PATH (string match -v $CONDA_PREFIX/bin $PATH)
+  type -q pbs-config; and set -lx PATH (string match -v (pbs-config --prefix)/bin $PATH)
+  type -q pyenv; and set -lx PATH (string match -v (pyenv root)/shims $PATH)
   command brew $argv
 end
